318f1cae82fd2773c8381989e4a60b284a58a409
mrcjssnr <mrcjssnr@gmail.com>
2024-03-22 00:29:45 +0800

    fixed links in javascript pt4

--- Smooth scrolling.js
+++ Smooth scrolling.js
@@ -2,12 +2,11 @@ const allLinks = document.querySelectorAll("a:link");
 
 allLinks.forEach(function (link) {
   link.addEventListener("click", function (e) {
-    // prevents reload
+    e.preventDefault(); // prevents reload
     const href = link.getAttribute("href");
 
     // Scroll back to top ||
     if (href === "#") {
-      e.preventDefault();
       window.scrollTo({
         top: 0,
         behavior: "smooth",
@@ -16,7 +15,6 @@ allLinks.forEach(function (link) {
 
     // Scroll to other links
     if (href !== "#" && href.startsWith("#")) {
-      e.preventDefault();
       const sectionEl = document.querySelector(href);
       sectionEl.scrollIntoView({ behavior: "smooth" });
     }
