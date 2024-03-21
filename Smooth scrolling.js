4124a8ab52f5d88dcd8b9dd6318fc0b588e215ef
mrcjssnr <mrcjssnr@gmail.com>
2024-03-22 00:16:07 +0800

    fixed links in javascript

--- Smooth scrolling.js
+++ Smooth scrolling.js
@@ -2,18 +2,20 @@ const allLinks = document.querySelectorAll("a:link");
 
 allLinks.forEach(function (link) {
   link.addEventListener("click", function (e) {
-    e.preventDefault(); // prevents reload
+    // prevents reload
     const href = link.getAttribute("href");
 
     // Scroll back to top ||
-    if (href === "#")
+    if (href === "#") {
       window.scrollTo({
         top: 0,
         behavior: "smooth",
       });
+    }
 
     // Scroll to other links
     if (href !== "#" && href.startsWith("#")) {
+      e.preventDefault();
       const sectionEl = document.querySelector(href);
       sectionEl.scrollIntoView({ behavior: "smooth" });
     }
