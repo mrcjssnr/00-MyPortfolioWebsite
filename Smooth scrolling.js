f8816a6bdd050fbcb8c3bcb729d266cc1232aa53
mrcjssnr <mrcjssnr@gmail.com>
2024-03-21 22:15:15 +0800

    added javascript for smooth scrolling

---
+++ Smooth scrolling.js
@@ -0,0 +1,25 @@
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault(); // prevents reload
+    const href = link.getAttribute("href");
+
+    // Scroll back to top ||
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    /* // Close mobile naviagtion
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open"); */
+  });
+});
